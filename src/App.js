8bc6e4d3fb7a3fc6e234c79f535b5d76589290b6
sharjeelyunus <sharjeel924@gmail.com>
2021-08-27 12:53:27 +0500

    📦 NEW: Header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
+import Header from './components/Header';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hack Club IUB</h1>
+      <header className="App-header">
+        <Header />
+      </header>
     </div>
   );
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUsers } from '@fortawesome/free-solid-svg-icons';
+import './Header.css';
+
+const Header = () => {
+  return (
+    <div className='header'>
+      <div className='header__img'>
+        <img src='https://assets.hackclub.com/flag-orpheus-top.svg' alt='' />
+      </div>
+      <div className='header__list'>
+        <ul>
+          <a href='#Home' className='header__icon'>
+            <li>Home</li>
+          </a>
+          <a href='#Team' className='header__icon'>
+            <li>Team</li>
+          </a>
+          <a href='#Rules' className='header__icon'>
+            <li>Rules</li>
+          </a>
+          <a href='#Faqs' className='header__icon'>
+            <li>Faqs</li>
+          </a>
+          <a href='/' className='redbutton'>
+            <li>
+              <FontAwesomeIcon icon={faUsers} />
+              Join Us
+            </li>
+          </a>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
