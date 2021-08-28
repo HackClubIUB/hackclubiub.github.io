998e3bb0d0109a2aef02deb9b03f18000e8883d4
sharjeelyunus <sharjeel924@gmail.com>
2021-08-28 13:06:17 +0500

    📦 NEW: Home

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import Header from './components/Header';
+import Home from './components/Home';
 
 function App() {
   return (
@@ -7,6 +8,7 @@ function App() {
       <header className="App-header">
         <Header />
       </header>
+      <Home />
     </div>
   );
 }
---
+++ src/components/Home.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import './Home.css';
+
+const Home = () => {
+  return (
+    <div className='home' id='Home'>
+      <div className='home__about'>
+        <h1>Welcome to Hack Club IUB!</h1>
+        <p>
+          Hack Club SKCET is the coding community of The Islamia University of
+          Bahawalpur. We are a chapter of hackclub.com. Hacker, technologist,
+          entrepreneur, coder, geek, pro or noob - Hack Club IUB welcomes you to
+          be a part of our fam. We encourage students to take part in various
+          competitive and collaborative coding events that take place across the
+          world. We maintain a active community where we discuss healthy topics
+          and grow together.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
